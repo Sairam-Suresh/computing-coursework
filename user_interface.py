eb192d7ed4900fe0d3eea2a764ed2860c9d71e7d
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2024-02-12 23:36:30 +0800

    Added import

--- user_interface.py
+++ user_interface.py
@@ -1,6 +1,7 @@
 # Done By: Ayaan
 
 from semantic_search import *
+import streamlit as st
 
 # Requires the user to acknowledge that the disclaimer
 internet_disclaimer = st.checkbox("Acknowledge that using this programme requires you to have an internet connection")
