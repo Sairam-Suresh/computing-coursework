c1c36c10ca32adbb3d352be3b8e57585d5fd6e82
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2024-02-16 13:29:23 +0800

    Fixed Lohith's Scraper

--- Scraping_Brightsparks.py
+++ Scraping_Brightsparks.py
@@ -1,4 +1,5 @@
 # This script was created in full by Lohith Ishan from S408
+# With slight assistance from Sairam Suresh from S401
 # Imports
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -10,15 +11,16 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import re
 import time
+from time import sleep
 import random
 
 
 def brightsparks():
-    #website being used to search scholarships
+    # website being used to search scholarships
     url = 'https://brightsparks.com.sg/searchScholarships.php'
     start = time.time()
 
-    #Opening Website
+    # Opening Website
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
     options.add_argument(
@@ -28,13 +30,12 @@ def brightsparks():
     driver.delete_all_cookies()
     driver.get(url)
 
-    #Exit the ad if it appears
+    # Exit the ad if it appears
     driver.find_element(By.CLASS_NAME, "close").click()
 
     # Select dropdown to choose scholarships based on university students
     Select(driver.find_element(By.ID, "eligibility")).select_by_value("4")
     driver.find_element(By.CLASS_NAME, "btn-ss").click()
-
 
     # Keeps clicking the show-more button to show all the scholarships
     while True:
@@ -53,11 +54,13 @@ def brightsparks():
         # Setup scholarship website
         opt = webdriver.ChromeOptions()
         opt.add_argument("--headless=new")
-        opt.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) ""AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36")
+        opt.add_argument(
+            "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) ""AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36")
         drive = webdriver.Chrome(service=Service(
             ChromeDriverManager().install()), options=opt)
         drive.delete_all_cookies()
         drive.get(link)
+        sleep(3)
 
         # Links that are paired with the content will be in this list
         paired_links = []
@@ -65,18 +68,22 @@ def brightsparks():
         # Variables
         save = drive.current_url
         possible_hrefs = ['//a[@href="scholarship.php"]', '//a[@href="sçcholarships.php"]', '//a[@href="scholar.php"]',
-             '//a[@href="mpa-scholarship-programme.php"]', '//a[@href="ss-scholarship.php"]', '//a[@href="uni.php"]',
-             '//a[@href="midterm.php?schid=10441"]', '//area[@href="scholarship.php"]','//a[@href="phd.php"]',
-             '//a[@href="scholarships_bcaundergrad.php"]', '//a[@href="csit-undergraduate-scholarship.php"]',
-             '//a[@href="our-scholarships.php"]',"scholarship-available.php", '//a[@href="#"]']
-
+                          '//a[@href="mpa-scholarship-programme.php"]', '//a[@href="ss-scholarship.php"]',
+                          '//a[@href="uni.php"]',
+                          '//a[@href="midterm.php?schid=10441"]', '//area[@href="scholarship.php"]',
+                          '//a[@href="phd.php"]',
+                          '//a[@href="scholarships_bcaundergrad.php"]',
+                          '//a[@href="csit-undergraduate-scholarship.php"]',
+                          '//a[@href="our-scholarships.php"]', "scholarship-available.php", '//a[@href="#"]']
 
         print(save)
+        sleep(3)
         try:
             if "aic" in save:
                 return paired_links
             print([a.get_attribute("href") for a in drive.find_elements(By.CSS_SELECTOR, "a") if
-                   (("view" in a.get_attribute("href") or "php" in a.get_attribute("href")) and "dukenus" in save) or ("view" in (a.get_attribute("href") and "php" in a.get_attribute("href")))])
+                   (("view" in a.get_attribute("href") or "php" in a.get_attribute("href")) and "dukenus" in save) or (
+                               "view" in (a.get_attribute("href") and "php" in a.get_attribute("href")))])
             if [a.get_attribute("href") for a in drive.find_elements(By.CSS_SELECTOR, "a") if
                 "view" in a.get_attribute("href") or "php" in a.get_attribute("href")]:
 
@@ -87,14 +94,17 @@ def brightsparks():
                     openlink = requests.get(i)
                     opensoup = BeautifulSoup(openlink.content, 'html.parser')
                     paired_links.append([openlink.url, re.sub(r"\n+", "\n", "".join(
-                        [i.text for i in opensoup.find("div", class_="scholarship_description")] if opensoup.find("div", class_="scholarship_description") is not None and "dukenus" in save else [i.text for i in opensoup.find_all("p")]))])
+                        [i.text for i in opensoup.find("div", class_="scholarship_description")] if opensoup.find("div",
+                                                                                                                  class_="scholarship_description") is not None and "dukenus" in save else [
+                            i.text for i in opensoup.find_all("p")]))])
                 return paired_links if "dukenus" not in save else paired_links[4:-1]
 
             # For certain websites, this code will take the information from the first page
             if "SNS" not in save and "dsg" not in save:
                 soup = BeautifulSoup(drive.page_source, 'html.parser')
                 try:
-                    paired_links.append([save, re.sub(r"\n+", "\n", "".join([i.text for i in soup.find("div", class_="col-md-12")]))])
+                    paired_links.append(
+                        [save, re.sub(r"\n+", "\n", "".join([i.text for i in soup.find("div", class_="col-md-12")]))])
                     drive.quit()
                     return paired_links
                 except:
@@ -126,26 +136,27 @@ def brightsparks():
                         drive.quit()
                         return ""
 
-
         # Wait for the code to load and opens new link
-        time.sleep(5+random.randrange(-2, 2))
+        time.sleep(5 + random.randrange(0, 4))
         soup = BeautifulSoup(drive.page_source, 'html.parser')
         save = drive.current_url
         print(save)
         drive.quit()
+        sleep(5)
 
         # This checks if there are any links related to scholarships within the current website
-        linklist = [a["href"] for a in soup.find_all('a', href=True) if "http" in a["href"] and "brightsparks" in a["href"] and a["href"]!= "https://brightsparks.com.sg/"]
+        linklist = [a["href"] for a in soup.find_all('a', href=True) if
+                    "http" in a["href"] and "brightsparks" in a["href"] and a["href"] != "https://brightsparks.com.sg/"]
 
         # If there are no links in the current website, It will get information from the current website
         # Else it will loop through the links in the website for each scholarship
         if not linklist:
             try:
-                if soup.find("div",id="collapseOne") is None:
+                if soup.find("div", id="collapseOne") is None:
                     desc = "".join([i.getText() for i in soup.find("div", id="scholarship_description")])
-                elif soup.find("div",id="inner-banner") is None:
+                elif soup.find("div", id="inner-banner") is None:
                     desc = "".join([i.getText() for i in soup.find("div", class_="inner-banner")])
-                elif soup.find("figure",class_="tabBlock"):
+                elif soup.find("figure", class_="tabBlock"):
                     desc = "".join([i.getText() for i in soup.find("figure", class_="tabBlock")])
                 paired_links.append([save, re.sub(r"\n+", "\n", desc)])
             except:
@@ -155,24 +166,37 @@ def brightsparks():
                 for i in range(len(linklist)):
                     openlink = requests.get(linklist[i])
                     opensoup = BeautifulSoup(openlink.content, 'html.parser')
-                    paired_links.append([openlink.url, re.sub(r"\n+", "\n", "".join([i.getText() for i in opensoup.find("div", id="scholarship_description")] if opensoup.find("div", id="collapseOne") is None else [i.getText() for i in opensoup.find("div", id="collapseOne")] if opensoup.find("div", id="wrapper") is None else [i.getText() for i in opensoup.find("div", id="scholarship_description")] if opensoup.find("div", id="collapseOne") is None else [i.getText() for i in opensoup.find("div", id="collapseOne")]))])
+                    paired_links.append([openlink.url, re.sub(r"\n+", "\n", "".join(
+                        [i.getText() for i in opensoup.find("div", id="scholarship_description")] if opensoup.find(
+                            "div", id="collapseOne") is None else [i.getText() for i in opensoup.find("div",
+                                                                                                      id="collapseOne")] if opensoup.find(
+                            "div", id="wrapper") is None else [i.getText() for i in opensoup.find("div",
+                                                                                                  id="scholarship_description")] if opensoup.find(
+                            "div", id="collapseOne") is None else [i.getText() for i in
+                                                                   opensoup.find("div", id="collapseOne")]))])
             except:
                 return ""
         return paired_links
 
 
-    # Get all the scholarships
-    elements = driver.find_elements(By.CLASS_NAME, 'col-span-11')
 
+
+
+    # Get all the scholarships
+    # elements = driver.find_elements(By.CLASS_NAME, 'col-span-11')
 
     # Extracts scholarships from the list of websites
     sitelist = []
+    sleep(1)
     information = []
-    for title in range(len(elements)):
-        elem = elements[title].find_element(By.TAG_NAME, "a")
+    for title in driver.find_elements(By.CLASS_NAME, 'col-span-11'):
+        elem = title.find_element(By.TAG_NAME, "a")
         site = elem.get_attribute('href').split("(")[0]
         if site not in sitelist and "jtc" not in site.lower():
             sitelist.append(site)
             information += websitescraper(site)
+
+
+
     print("done")
     return [i for i in information if type(i) == type([])]
